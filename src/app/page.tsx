8681afe1d04d116cd385a2efe6113785c239547b
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-05-01 09:49:35 +0200

    Implement NEXT.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Todo from "@/Todo";
 
 export default function Home() {
   return (
-   
-          <h1>Latifa & Mustapha</h1>
-        
+    <Todo/>
   )
 }
