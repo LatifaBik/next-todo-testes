153698f56b374e293b42a77a16f3c454955d8c65
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-05-15 13:54:16 +0200

    Add cypress boilerplate

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import "./index.css"
+import "./index.css";
 
 import { useEffect, useState, type FC } from "react";
 
-import TaskCounter from './components/TaskCounter';
-import TaskForm from './components/TaskForm';
-import TaskList from './components/TaskList';
+import TaskCounter from './app/components/TaskCounter';
+import TaskForm from './app/components/TaskForm';
+import TaskList from './app/components/TaskList';
 import type { Task } from "./types";
 
-const Todo: FC = () => {
+const Todo = () => {
   const [items, setItems] = useState<Task[]>([]);
 
   useEffect(() => {
@@ -28,19 +28,22 @@ const Todo: FC = () => {
       <h1 className="app__title">My ToDo</h1>
       <TaskCounter items={items} />
       <TaskForm onCreateItem={async (text) => {
-        const newItem = {
-          label: text,
-          completed: false,
-        };
+  const newItem: Task = {
+    label: text,
+    completed: false,
+  };
 
-        await fetch('/api/tasks', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(newItem),
-        });
+    await fetch("/api/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newItem),
+    });
 
-        setItems([...items, newItem]);
-      }} />
+    setItems([...items, newItem]);
+  }}
+/>
+
+      
       <TaskList
         items={items}
         onToggleItem={(index, completed) => {
@@ -54,6 +57,7 @@ const Todo: FC = () => {
       />
     </main>
   );
+
 }
 
 export default Todo
---
+++ src/app/components/TaskForm.tsx
@@ -0,0 +1,31 @@
+import { useState, type FC } from 'react';
+
+type Props = {
+  onCreateItem: (text: string) => void;
+}
+
+const TaskForm: FC<Props> = ({ onCreateItem}) => {
+  const [text, setText] = useState('');
+  return (
+<form
+  className="taskForm"
+  onSubmit={(ev) => {
+    ev.preventDefault();
+    console.log("click", text);
+    onCreateItem(text);
+  }}
+>
+      <input
+        className="taskForm__input"
+        name="taskText"
+        type="text"
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+      <button className="taskForm__submitButton" type="submit">OK</button>
+      <p className="taskForm__error"></p>
+    </form>
+  );
+}
+
+export default TaskForm;
