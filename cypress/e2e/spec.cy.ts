d6079fd2d32854fe97185e543e63ca1e927c29cd
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-05-17 15:36:37 +0200

    Add task to reset database

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,10 @@
 describe('todo app', () => {
+    
   it('can create a new task', () => {
+     cy.task('resetDatabase');
+    
     cy.visit('http://localhost:3001')
+    
     cy.get('input').focus().type('Learn e2e testing');
 
     cy.get('[data-testid=taskForm__submitButton]').click();
@@ -9,5 +13,8 @@ describe('todo app', () => {
 
     cy.visit('http://localhost:3001');
     cy.get('.taskList').should('contain', 'Learn e2e testing');
+   
+ 
+    
   })
 })
